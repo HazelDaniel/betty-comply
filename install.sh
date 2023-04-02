637d01d37c276eec150eebf31f12546029cc8b41
HazelDaniel <hazeldaniel856@gmail.com>
2023-04-02 14:14:34 -0500

    added the watch script to the installation environment

--- install.sh
+++ install.sh
@@ -8,8 +8,10 @@ fi
 
 COMPLY_ALL="_comply_all"
 BETTY_COMPLY="betty-comply"
+BETTY_WATCH_COMPLY="_watch_n_comply"
 COMPLY_ALL_SYM="_complyAll"
 BETTY_COMPLY_SYM="_bettyComply"
+BETTY_WATCH_COMPLY_SYM="_watchComply"
 
 
 
@@ -23,13 +25,15 @@ mkdir -p "${APP_PATH}"
 
 cp "${COMPLY_ALL}.sh" "${APP_PATH}/${COMPLY_ALL}"
 cp "${BETTY_COMPLY}.pl" "${APP_PATH}/${BETTY_COMPLY}"
-
+cp "${BETTY_WATCH_COMPLY}.sh" "${APP_PATH}/${BETTY_WATCH_COMPLY}"
 
 chmod +x "${APP_PATH}/${COMPLY_ALL}"
 chmod +x "${APP_PATH}/${BETTY_COMPLY}"
+chmod +x "${APP_PATH}/${BETTY_WATCH_COMPLY}"
 
 ln -s "${APP_PATH}/${COMPLY_ALL}" "${BIN_PATH}/${COMPLY_ALL_SYM}"
 ln -s "${APP_PATH}/${BETTY_COMPLY}" "${BIN_PATH}/${BETTY_COMPLY_SYM}"
+ln -s "${APP_PATH}/${BETTY_WATCH_COMPLY}" "${BIN_PATH}/${BETTY_WATCH_COMPLY_SYM}"
 
 echo -e "Installing man pages.."
 
